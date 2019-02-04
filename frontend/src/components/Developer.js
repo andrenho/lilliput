4788edccd46f5aaf9107ef57ad64edceb5956810
andrenho <xmate156>
2019-02-04 18:42:16 -0200

    .

--- frontend/src/components/Developer.js
+++ frontend/src/components/Developer.js
@@ -3,11 +3,20 @@ import HexViewer from './HexViewer';
 import SourceEditor from './SourceEditor';
 
 class Developer extends Component {
+
+    outputBinary = Uint8Array.of(1, 2, 3);
+
+    state = {}
+
+    compile() {
+        
+    }
+
     render() {
         return (<div>
             <SourceEditor />
             <button>Compile</button>
-            <HexViewer />
+            <HexViewer binary={this.outputBinary} />
         </div>);
     }
 }
--- frontend/src/components/HexViewer.js
+++ frontend/src/components/HexViewer.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 
 class HexViewer extends Component {
+
+    upstreamData() {
+        return this.props.binary.map(v => v).join(', ');
+    }
+
     render() {
-        return <textarea></textarea>;
+        return <textarea readOnly value={ this.upstreamData() } />;
     }
 }
 
